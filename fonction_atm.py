e32e496080917db848d2e1c6aa28e0f43f69750d
Krikiba <m.krikiba@gmail.com>
2018-03-08 10:03:43 +0000

    Ajout boucle for

--- fonction_atm.py
+++ fonction_atm.py
@@ -3,9 +3,9 @@
 def withdraw(request):
 	i=0
 	liste=[100,50,10,5,4,3,2,1]
-	while len(liste):
-		if request>=liste[i] :
-			request-=liste[i]
+	for lis in liste:
+		if request>=lis :
+			request-=lis
 			return request
 		else :	
 			i+=1
@@ -14,7 +14,9 @@ def withdraw(request):
 
 
 def atm(money,request):
+	balence=request
 	print "Current balance =" +str(money)
+
 	while request>0:
 		if request>money :
 			print ("le montant n'existe pas en ATM")
@@ -24,5 +26,7 @@ def atm(money,request):
 			request=withdraw(request)	
 			print "give " +str(d-request)
 
-atm(500,499)			
+	return money-balence		
+
+print atm(500,455)			
 		
